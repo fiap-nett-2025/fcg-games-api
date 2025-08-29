d2bb9550f30fafe967fa2cabec04590539b63a3a
betomperez <betomperez@gmail.com>
2025-08-29 17:56:42 -0300

    Add GameRepository tests and database context setup
    Introduces `GameRepositoryTests` for unit testing the `GameRepository` class with in-memory database support. Updates `Tests.Infra.csproj` to target .NET 8.0 and include necessary testing packages. Modifies the solution file to include the new `Infra` and `Tests.Infra` projects. Defines `GameDbContext` with a `DbSet<Game>` and configures the entity model. Updates `Infra.csproj` with Entity Framework Core references. Implements `IGameRepository` in `GameRepository` and adds `GameSeeding` for initial game data.

---
+++ Tests.Infra/Repository/RepositoryTests.cs
@@ -0,0 +1,109 @@
+using Domain.Entities;
+using Domain.Enums;
+using Infra.Data;
+using Infra.Repository;
+using Microsoft.EntityFrameworkCore;
+
+namespace Tests.Infra.Repository;
+
+public class GameRepositoryTests
+{
+    private readonly GameDbContext _context;
+    private readonly GameRepository _repository;
+
+    public GameRepositoryTests()
+    {
+        var options = new DbContextOptionsBuilder<GameDbContext>()
+            .UseInMemoryDatabase(databaseName: "TestDatabase")
+            .Options;
+
+        _context = new GameDbContext(options);
+        _repository = new GameRepository(_context);
+    }
+
+    [Fact]
+    public async Task AddAsync_ValidGame_ShouldAddGameToDatabase()
+    {
+        // Arrange
+        var game = new Game("Tetris", 9.99M, "Puzzle Clássico", [GameGenre.Puzzle]);
+
+        // Act
+        await _repository.AddAsync(game);
+        var savedGame = await _repository.GetBy(g => g.Title == game.Title);
+
+        // Assert
+        Assert.NotNull(savedGame);
+        Assert.Equal("Tetris", savedGame.Title);
+    }
+
+    [Fact]
+    public async Task DeleteAsync_DeleteExistingGame_ShouldRemoveGameFromDatabase()
+    {
+        // Arrange
+        var game = new Game("The Witcher 3", 49.99M, "RPG de Ação", [GameGenre.RPG, GameGenre.Adventure]);
+        await _repository.AddAsync(game);
+        var gameToDelete = _repository.GetBy(g => g.Title == "The Witcher 3");
+        _context.ChangeTracker.Clear();
+
+        // Act
+        await _repository.DeleteAsync(gameToDelete.Id);
+
+        // Assert
+        Assert.Null(await _repository.GetBy(g => g.Id.Equals(gameToDelete.Id)));
+    }
+
+    [Fact]
+    public async Task GetAllAsync_ShouldReturnAllGames()
+    {
+        // Arrange
+        var games = new List<Game>
+        {
+            new Game("Sonic 2", 49.99M, "Plataforma 2d", [GameGenre.Platformer]),
+            new Game("Deus Ex", 59.99M, "RPG de Ação", [GameGenre.Platformer, GameGenre.FPS]),
+            new Game("Resident Evil 3", 39.99M, "Survival Horror", [GameGenre.Horror])
+        };
+        _context.Games.AddRange(games);
+        _context.SaveChanges();
+        // Act
+        var result = await _repository.GetAllAsync();
+
+        // Assert
+        Assert.Equal(_context.Games.Count(), result.Count());
+        Assert.Contains(result, g => g.Title == "Sonic 2");
+        Assert.Contains(result, g => g.Title == "Deus Ex");
+        Assert.Contains(result, g => g.Title == "Resident Evil 3");
+    }
+
+    [Fact]
+    public async Task GetBy_ByTitle_ShouldReturnGame()
+    {
+        // Arrange
+        var game = new Game("Cyberpunk 2077", 99.99M, "RPG de Ação", [GameGenre.RPG, GameGenre.FPS, GameGenre.Adventure]);
+        await _repository.AddAsync(game);
+
+        // Act
+        var result = await _repository.GetBy(g => g.Title == "Cyberpunk 2077");
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal("Cyberpunk 2077", result.Title);
+    }
+
+    [Fact]
+    public async Task UpdateAsync_ModifyPrice_ShouldModify()
+    {
+        // Arrange
+        var game = new Game("Resident Evil 4", 39.99M, "Survival Horror", [GameGenre.Horror]);
+        await _repository.AddAsync(game);
+        var newPrice = 59.99M;
+
+        // Act
+        game.Price = newPrice;
+        await _repository.UpdateAsync(game);
+
+        // Assert
+        var updatedGame = await _repository.GetBy(g => g.Title == "Resident Evil 4");
+        Assert.NotNull(updatedGame);
+        Assert.Equal(newPrice, updatedGame.Price);
+    }
+}
---
+++ src/Infra/Data/GameDbContext.cs
@@ -0,0 +1,43 @@
+﻿using Domain.Entities;
+using Domain.Enums;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Data;
+
+public class GameDbContext : DbContext
+{
+    public DbSet<Game> Games { get; set; }
+    public GameDbContext(DbContextOptions<GameDbContext> options) : base(options) { }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Game>(entity =>
+        {
+            entity.ToTable("games");
+
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Id).ValueGeneratedOnAdd();
+
+            entity.HasIndex(e => e.Title).IsUnique();
+            entity.Property(e => e.Title)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            entity.Property(e => e.Price)
+                .IsRequired()
+                .HasColumnType("decimal(18,2)");
+
+            entity.Property(e => e.Description)
+                .IsRequired()
+                .HasMaxLength(500);
+
+            entity.Property(e => e.Genre)
+                .HasMaxLength(500)
+                .IsRequired()
+                .HasConversion(
+                     v => string.Join(',', v), // Convert List<GameGenre> to string for storage
+                     v => v.Split(',', StringSplitOptions.RemoveEmptyEntries).Select(gg => Enum.Parse<GameGenre>(gg)).ToList() // Convert string back to List<GameGenre>
+                );
+        });
+    }
+}

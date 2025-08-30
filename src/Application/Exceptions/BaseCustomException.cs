66fe61fc60c0fb1450411650609002765674d726
betomperez <betomperez@gmail.com>
2025-08-30 11:40:19 -0300

    Update project to .NET 8.0 and add custom exceptions
    - Updated `Application.csproj` to target .NET 8.0 and added package references for `Microsoft.AspNetCore.Http.Abstractions` and `Newtonsoft.Json`.
    - Introduced `BaseCustomException` as a base class for custom exceptions.
    - Added `BusinessErrorDetailsException`, `InvalidTokenException`, `NotFoundException`, and `TechnicalErrorDetailsException` for specific error handling.
    - Implemented `ExceptionMiddleware` to manage exceptions and return structured JSON responses.
    - Created a generic `ResponseWrapper<T>` class for API responses, including methods for success and failure responses.

---
+++ src/Application/Exceptions/BaseCustomException.cs
@@ -0,0 +1,18 @@
+﻿namespace Application.Exceptions;
+
+public abstract class BaseCustomException : Exception
+{
+    public int StatusCode { get; }
+
+    protected BaseCustomException(int statusCode, string message, Exception? innerException = null)
+        : base(message, innerException)
+    {
+        StatusCode = statusCode;
+    }
+
+    protected BaseCustomException(int statusCode, string message)
+        : base(message)
+    {
+        StatusCode = statusCode;
+    }
+}

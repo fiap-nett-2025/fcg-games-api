2967d2d0b2bebe7846603794b7b5940e1479be02
betomperez <betomperez@gmail.com>
2025-08-30 12:04:33 -0300

    Add InitialCreate migration for games table
    This commit introduces a new migration class `InitialCreate` to set up the database schema for a `games` table in the Entity Framework Core context. The migration defines the table structure with columns for `Id`, `Title`, `Price`, `Description`, and `Genre`, including their data types and constraints. An index is created on the `Title` column to ensure uniqueness. Additionally, a partial class for the migration is generated, along with a model snapshot class that captures the current state of the model for future migrations.

---
+++ src/Infra/Migrations/20250830144640_InitialCreate.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "games",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    Genre = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_games", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_games_Title",
+                table: "games",
+                column: "Title",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "games");
+        }
+    }
+}

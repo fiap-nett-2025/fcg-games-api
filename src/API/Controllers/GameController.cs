ca92d82829a2d8ad2020f288d07a95c3fe14bef8
betomperez <betomperez@gmail.com>
2025-08-30 11:42:56 -0300

    Add API project and configure Swagger documentation
    - Updated `.dockerignore` to exclude unnecessary files.
    - Added new "API" project to the solution with relevant settings.
    - Created `dotnet-tools.json` for tool configuration.
    - Defined project settings in `API.csproj`, including dependencies.
    - Added sample HTTP request in `API.http`.
    - Configured Swagger in `SwaggerConfiguration.cs` for API documentation.
    - Implemented response filters for Swagger in `SwaggerResponseGenericExampleFilter.cs` and `SwaggerResponseWrapperOperationFilter.cs`.
    - Created `ApiBaseController.cs` for common response methods.
    - Developed `GameController.cs` for game management endpoints.
    - Defined multi-stage build process in `Dockerfile`.
    - Updated `Program.cs` for service configuration and middleware setup.
    - Modified `launchSettings.json` for application launch profiles.
    - Configured logging in `appsettings.Development.json` and `appsettings.json`.

---
+++ src/API/Controllers/GameController.cs
@@ -0,0 +1,65 @@
+﻿using API.Controllers;
+using Application.DTOs;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class GamesController : ApiBaseController
+    {
+        private readonly IGameService _gameService;
+
+        public GamesController(IGameService gameService)
+        {
+            _gameService = gameService;
+        }
+
+        [HttpGet]
+        [Authorize(Policy = "Authenticated")]
+        public async Task<IActionResult> GetGames()
+        {
+            var games = await _gameService.GetAllGamesAsync();
+
+            return Success(games, "Lista de jogos retornada com sucesso.");
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetGame(int id)
+        {
+            var game = await _gameService.GetGameByIdAsync(id);
+
+            return Success(game, "Jogo encontrado.");
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> CreateGame(CreateGameDTO model)
+        {
+            var game = await _gameService.CreateGameAsync(model);
+            return CreatedResponse(game, "Jogo criado com sucesso.");
+        }
+
+
+        [HttpPatch("{id}")]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> PartialUpdateGame(int id, UpdateGameDTO model)
+        {
+            await _gameService.UpdateGameAsync(id, model);
+            return Success("Jogo atualizado com sucesso!");
+        }
+
+
+        [HttpDelete("{id:int}")]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> DeleteGame([FromRoute] int id)
+        {
+
+            await _gameService.DeleteGameAsync(id);
+            return NoContent();
+        }
+    }
+}

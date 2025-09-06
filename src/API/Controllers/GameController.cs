12b2b430803f117cdd026ca7f4215143739c11cb
betomperez <betomperez@gmail.com>
2025-09-06 12:06:03 -0300

    Enhance controllers with routing and authorization
    Added routing, API controller, and authorization attributes to `GameController.cs` and `PromotionController.cs`. Restructured constructors and methods for improved readability and maintainability. Standardized formatting across both controllers. Renamed `PartialUpdateGame` to `PartialUpdatePromotion` in `PromotionController.cs` for better context alignment.

--- src/API/Controllers/GameController.cs
+++ src/API/Controllers/GameController.cs
@@ -1,65 +1,63 @@
-﻿using API.Controllers;
-using Application.DTOs;
+﻿using Application.DTOs;
 using Application.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace API.Controllers
+namespace API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+[Authorize]
+public class GamesController : ApiBaseController
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    [Authorize]
-    public class GamesController : ApiBaseController
+    private readonly IGameService _gameService;
+
+    public GamesController(IGameService gameService)
     {
-        private readonly IGameService _gameService;
+        _gameService = gameService;
+    }
 
-        public GamesController(IGameService gameService)
-        {
-            _gameService = gameService;
-        }
+    [HttpGet]
+    [Authorize(Policy = "Authenticated")]
+    public async Task<IActionResult> GetGames()
+    {
+        var games = await _gameService.GetAllGamesAsync();
 
-        [HttpGet]
-        [Authorize(Policy = "Authenticated")]
-        public async Task<IActionResult> GetGames()
-        {
-            var games = await _gameService.GetAllGamesAsync();
+        return Success(games, "Lista de jogos retornada com sucesso.");
+    }
 
-            return Success(games, "Lista de jogos retornada com sucesso.");
-        }
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> GetGame(int id)
+    {
+        var game = await _gameService.GetGameByIdAsync(id);
 
-        [HttpGet("{id:int}")]
-        public async Task<IActionResult> GetGame(int id)
-        {
-            var game = await _gameService.GetGameByIdAsync(id);
+        return Success(game, "Jogo encontrado.");
+    }
 
-            return Success(game, "Jogo encontrado.");
-        }
-
-        [HttpPost]
-        [Authorize(Roles = "Admin")]
-        public async Task<IActionResult> CreateGame(CreateGameDTO model)
-        {
-            var game = await _gameService.CreateGameAsync(model);
-            return CreatedResponse(game, "Jogo criado com sucesso.");
-        }
+    [HttpPost]
+    [Authorize(Roles = "Admin")]
+    public async Task<IActionResult> CreateGame(CreateGameDTO model)
+    {
+        var game = await _gameService.CreateGameAsync(model);
+        return CreatedResponse(game, "Jogo criado com sucesso.");
+    }
 
 
-        [HttpPatch("{id}")]
-        [Authorize(Roles = "Admin")]
-        public async Task<IActionResult> PartialUpdateGame(int id, UpdateGameDTO model)
-        {
-            await _gameService.UpdateGameAsync(id, model);
-            return Success("Jogo atualizado com sucesso!");
-        }
+    [HttpPatch("{id}")]
+    [Authorize(Roles = "Admin")]
+    public async Task<IActionResult> PartialUpdateGame(int id, UpdateGameDTO model)
+    {
+        await _gameService.UpdateGameAsync(id, model);
+        return Success("Jogo atualizado com sucesso!");
+    }
 
 
-        [HttpDelete("{id:int}")]
-        [Authorize(Roles = "Admin")]
-        public async Task<IActionResult> DeleteGame([FromRoute] int id)
-        {
+    [HttpDelete("{id:int}")]
+    [Authorize(Roles = "Admin")]
+    public async Task<IActionResult> DeleteGame([FromRoute] int id)
+    {
 
-            await _gameService.DeleteGameAsync(id);
-            return NoContent();
-        }
+        await _gameService.DeleteGameAsync(id);
+        return NoContent();
     }
 }

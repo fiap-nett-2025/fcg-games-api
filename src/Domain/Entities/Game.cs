01a955ab54bba38e310c5408d50e23e647ec8c50
betomperez <betomperez@gmail.com>
2025-09-06 15:57:29 -0300

    Refactored entities to enforce immutability.

--- src/Domain/Entities/Game.cs
+++ src/Domain/Entities/Game.cs
@@ -4,11 +4,11 @@ namespace Domain.Entities;
 
 public class Game
 {
-    public int Id { get; set; }
-    public string Title { get; set; }
-    public decimal Price { get; set; }
-    public string Description { get; set; }
-    public List<GameGenre> Genre { get; set; }
+    public int Id { get; private set; }
+    public string Title { get; private set; } = null!;
+    public decimal Price { get; private set; }
+    public string Description { get; private set; } = null!;
+    public List<GameGenre> Genre { get; private set; } = null!;
 
     public Game(string title, decimal price, string description, List<GameGenre> genre)
     {
@@ -27,25 +27,49 @@ public class Game
     public static void ValidateTitle(string? title)
     {
         if (string.IsNullOrWhiteSpace(title))
-            throw new Exception("Título não pode ser vazio ou nulo.");
+            throw new ArgumentException("Título não pode ser vazio ou nulo.");
     }
 
     public static void ValidatePrice(decimal? price)
     {
         if (price is null or < 0)
-            throw new Exception("Preço não pode ser negativo.");
+            throw new ArgumentException("Preço não pode ser negativo.");
     }
 
     public static void ValidateDescription(string? description)
     {
         if (string.IsNullOrWhiteSpace(description))
-            throw new Exception("Descrição não pode ser vazio ou nulo.");
+            throw new ArgumentException("Descrição não pode ser vazio ou nulo.");
     }
 
     public static void ValidateGenreList(List<GameGenre>? genre)
     {
         if (genre == null || genre.Count == 0)
-            throw new Exception("Genero não pode ser vazio ou nulo.");
+            throw new ArgumentException("Genero não pode ser vazio ou nulo.");
+    }
+
+    public void UpdateTitle(string title)
+    {
+        ValidateTitle(title);
+        Title = title; 
+    }
+
+    public void UpdatePrice(decimal price)
+    {
+        ValidatePrice(price);
+        Price = price;
+    }
+
+    public void UpdateDescription(string description)
+    {
+        ValidateDescription(description);
+        Description = description;
+    }
+
+    public void UpdateGenre(List<GameGenre> genre)
+    {
+        ValidateGenreList(genre);
+        Genre = genre;
     }
 }
 

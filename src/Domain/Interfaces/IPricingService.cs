cef97ed46eb034ad2b3d74d03d56b0e80c8ca0ad
betomperez <betomperez@gmail.com>
2025-09-06 21:42:48 -0300

    Created a pricing service to calculate the best price for a game.

---
+++ src/Domain/Interfaces/IPricingService.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Entities;
+
+namespace Domain.Interfaces;
+
+public interface IPricingService
+{
+    decimal CalculateFinalPrice(Game game, IEnumerable<Promotion> activePromotions);
+}

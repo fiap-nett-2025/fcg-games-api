51d7342f7841567ac9241fee724950b96bdf7ab9
betomperez <betomperez@gmail.com>
2025-09-20 18:55:26 -0300

    Add DTOs and services for cart and payment management
    This commit introduces several Data Transfer Objects (DTOs) for managing cart and payment functionalities, including `CartDTO`, `CreateCartDTO`, `PaymentResult`, and `UpdateCartDTO`.
    
    It defines the `ICartService` and `IPaymentService` interfaces, promoting a clean separation of concerns. The `CartService` and `PaymentService` classes implement these interfaces, providing methods for cart management and payment processing, respectively.
    
    Additionally, the `UserService` class is introduced to check user existence via an API call. These changes enhance the application's structure by adhering to best practices in software design, such as dependency injection and separation of concerns.

---
+++ src/Application/DTOs/UpdateCartDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace Application.DTOs;
+
+public class UpdateCartDTO
+{
+    public string? UserId { get; set; }
+    public List<int>? GameIds { get; set; } = new();
+}
---
+++ src/Application/Services/UserService.cs
@@ -0,0 +1,26 @@
+﻿using Application.Interfaces;
+
+namespace Application.Services;
+
+public class UserService : IUserService
+{
+    private readonly HttpClient _httpClient;
+
+    public UserService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<bool> UserExistAsync(string userId)
+    {
+        try
+        {
+            var response = await _httpClient.GetAsync($"api/Users/{userId}");
+            return response.IsSuccessStatusCode;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+}

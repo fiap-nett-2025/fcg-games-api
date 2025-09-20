926b0a1d4b54c6aa05ebde8c1078279dd64ca19f
betomperez <betomperez@gmail.com>
2025-09-20 18:53:28 -0300

    Created entity Cart and a repository interface for it.

---
+++ src/Domain/Interfaces/ICartRepository.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+
+namespace Domain.Interfaces;
+
+public interface ICartRepository
+{
+    Task<IEnumerable<Cart>> GetAllAsync();
+    Task<Cart?> GetByIdAsync(string userId);
+    Task AddAsync(Cart cart);
+    Task UpdateAsync(Cart cart);
+    Task DeleteAsync(string userId);
+}

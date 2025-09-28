1ff6729865925348568eb5456d15b73724ba90f3
Fernanda Rocha Miranda <fernandamirandacursos@gmail.com>
2025-09-28 12:15:38 -0300

    ajuste no docker-compose e migration

--- src/FCG.Game.Infra.Data/Migrations/20250928150439_removing-cart-and-game-tables.Designer.cs
+++ src/FCG.Game.Infra.Data/Migrations/20250928150439_removing-cart-and-game-tables.Designer.cs
@@ -4,6 +4,7 @@ using FCG.Game.Infra.Data.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infra.Migrations
 {
     [DbContext(typeof(FcgGameDbContext))]
-    partial class GameDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250928150439_removing-cart-and-game-tables")]
+    partial class removingcartandgametables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

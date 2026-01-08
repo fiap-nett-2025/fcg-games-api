c676e1ccb6dc093cefda7bac2bd8fa5dc1d39139
Fernanda Rocha Miranda <fernandamirandacursos@gmail.com>
2026-01-08 02:19:45 -0300

    ajuste

--- FCG.Games.Infra/Persistence/Documents/GameDocument.cs
+++ FCG.Games.Infra/Persistence/Documents/GameDocument.cs
@@ -9,12 +9,12 @@ public class GameDocument
     public string Title { get; set; } = string.Empty;
     public decimal Price { get; set; }
     public string Description { get; set; } = string.Empty;
-    public List<GameGenre> Genre { get; set; } = [];
+    public List<GameGenre> Genres { get; set; } = [];
     public int Popularity { get; set; }
 
     // Método para converter para a entidade de domínio
     public Game ToEntity(string id)
     {
-        return Game.Reconstruct(id, Title, Price, Description, Genre, Popularity);
+        return Game.Reconstruct(id, Title, Price, Description, Genres, Popularity);
     }
 }
